3eef5c8b09874d711b889d4164055a9a318d4c95
danwin <dmitry.zimoglyadov@gmail.com>
2020-05-09 11:21:35 +0300

    ci: deploy script

---
+++ ci/prepare_deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eux
+
+export CRATE_NAME=$1
+name="$CRATE_NAME-$TRAVIS_TAG-$TARGET"
+mkdir -p "$name"
+cp "target/$TARGET/release/$CRATE_NAME" "$name/"
+# cp cargo-crev/README.md LICENSE* "$name/"
+tar czvf "$name.tar.gz" "$name"
+
+# Get the sha-256 checksum w/o filename and newline
+echo -n $(shasum -ba 256 "$name.tar.gz" | cut -d " " -f 1) > "$name.tar.gz.sha256"
